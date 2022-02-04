27297a55c257a0a5cfef3c5a106080ca974e822f
Onki Eceron <pratamaonki@gmail.com>
2022-02-04 14:40:28 +0700

    git commit -m "first stage"

---
+++ blog.js
@@ -0,0 +1,117 @@
+let blogs = [];
+
+function addBlog(event) {
+  event.preventDefault();
+
+  let title = document.getElementById("input-blog-title").value;
+  let content = document.getElementById("input-blog-content").value;
+  let image = document.getElementById("input-blog-image");
+
+  image = URL.createObjectURL(image.files[0]);
+
+  let blog = {
+    author: "Rhoma Irama",
+    title: title,
+    content,
+    image,
+    postedAt: new Date(),
+  };
+
+  blogs.push(blog);
+
+  renderBlog();
+}
+
+function renderBlog() {
+  let lengthData = blogs.length;
+
+  console.log(blogs[0].image);
+
+  let blogContainer = document.getElementById("contents");
+
+  // blogContainer = "data-default, data1, data2"
+
+  blogContainer.innerHTML = firstBlogContent();
+
+  // blogContainer = "data-default"
+
+  let i = 0;
+  for (i; i < lengthData; i++) {
+    blogContainer.innerHTML += `
+        <div class="blog-list-item">
+            <div class="blog-image">
+            <img src="${blogs[i].image}" alt="" />
+            </div>
+            <div class="blog-content">
+            <div class="btn-group">
+                <button class="btn-edit">Edit Post</button>
+                <button class="btn-post">Post Blog</button>
+            </div>
+            <h1>
+                <a href="blog-detail.html" target="_blank">
+                    ${blogs[i].title}
+                </a>
+            </h1>
+            <div class="detail-blog-content">
+                ${blogs[i].postedAt} | ${blogs[i].author}
+            </div>
+            <p>
+                ${blogs[i].content} 
+            </p>
+            </div>
+        </div>`;
+  }
+
+  // blogContainer = "data-default, data1, data2, data3"
+}
+
+function firstBlogContent() {
+  return `
+    <div class="blog-list-item">
+    <div class="blog-image">
+      <img src="assets/blog-img.png" alt="" />
+    </div>
+    <div class="blog-content">
+      <div class="btn-group">
+        <button class="btn-edit">Edit Post</button>
+        <button class="btn-post">Post Blog</button>
+      </div>
+      <h1>
+        <a href="blog-detail.html" target="_blank">Pasar Coding di Indonesia Dinilai Masih Menjanjikan</a>
+      </h1>
+      <div class="detail-blog-content">
+        12 Jul 2021 22:30 WIB | Ichsan Emrald Alamsyah
+      </div>
+      <p>
+        Ketimpangan sumber daya manusia (SDM) di sektor digital masih
+        menjadi isu yang belum terpecahkan. Berdasarkan penelitian
+        ManpowerGroup, ketimpangan SDM global, termasuk Indonesia,
+        meningkat dua kali lipat dalam satu dekade terakhir. Lorem ipsum,
+        dolor sit amet consectetur adipisicing elit. Quam, molestiae
+        numquam! Deleniti maiores expedita eaque deserunt quaerat! Dicta,
+        eligendi debitis?
+      </p>
+    </div>
+  </div>
+    `;
+}
+function getFullTime() {
+  let date = time.getDate();
+  let month = time.getMonth;
+  let year = time.FullYear;
+  let hours = time.Hours;
+  let minutes = time.Minutes;
+  console.log(date);
+  console.log(month);
+  console.log(year);
+  console.log(hours);
+  console.log(minutes);
+  return `${date} ${month} ${year} ${hours}:${minutes} WIB`;
+}
+Function getDistanceTime(time){
+  const distance = new Date()- new Date()
+  const milisecond = 1000;
+  const secondsInMinute =60;
+  const minutesInHour =60;
+  const secondInHour = secondsInMinute * minutesInHour
+}
---
+++ submission.js
@@ -0,0 +1,32 @@
+// const name = "Onki";
+// let myName = "Pratama";
+// const age = 27;
+// console.log(name);
+// console.log(name + " " + myName + " " + age);
+// console.log(`nama saya ${name} umur saya ${age}`);
+
+// // function
+// function showdata() {
+//   let myName = "Onki";
+//   console.log(myName);
+// }
+
+function showData() {
+  // dom mengambil data
+  let name = document.getElementById("input-name").value;
+  let name = document.getElementById("input-email").value;
+  let name = document.getElementById("input-phone").value;
+  let name = document.getElementById("input-subject").value;
+  let name = document.getElementById("input-message").value;
+  console.log(allValue);
+  // //dom mengerim data
+  // document.getElementById("input-name").value = "";
+}
+
+// if (name == "lapar") {
+//   console.log("yo makan");
+// } else if (name == makan) {
+//   console.log("kenyang");
+// } else {
+//   console.log("ya mbuh");
+// }
